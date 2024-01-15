d0b2e65346b54d3cae5fe75b02602c3e5ae938ff
Theodor Waalberg <twaal@bu.edu>
2024-01-15 21:45:14 +0100

    add frontend

--- lumify_app/app.py
+++ lumify_app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask import session
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
@@ -19,6 +20,35 @@ db.create_all()
 @app.route('/')
 def index():
     return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        hashed_password = generate_password_hash(password, method='sha256')
+        
+        new_user = User(username=username, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('index'))
+
+    return render_template('register.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+
+    user = User.query.filter_by(username=username).first()
+
+    if user and check_password_hash(user.password, password):
+        session['user_id'] = user.id
+        return "Logged in successfully"
+    else:
+        return "Login failed"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
