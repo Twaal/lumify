a4c1c49fcb515abeae5ebdd866f54924917e516d
Theodor Waalberg <twaal@bu.edu>
2024-01-15 21:57:14 +0100

    simplify app.py

--- lumify_app/app.py
+++ lumify_app/app.py
@@ -1,57 +1,25 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask import session
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable a Flask-SQLAlchemy feature that causes a warning
+app.secret_key = 'your_secret_key'  # Change this to a random secret key
+
 db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
-    profile_photo = db.Column(db.String(120))
-    vector_embedding = db.Column(db.String(200))  # You might need to adjust this based on your data
 
 # Create the database
-db.create_all()
+with app.app_context():
+    db.init_app(app)
+    db.create_all()
 
-@app.route('/')
-def index():
-    return render_template('login.html')
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        hashed_password = generate_password_hash(password, method='sha256')
-        
-        new_user = User(username=username, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
-
-        return redirect(url_for('index'))
-
-    return render_template('register.html')
-
-@app.route('/login', methods=['POST'])
-def login():
-    username = request.form['username']
-    password = request.form['password']
-
-    user = User.query.filter_by(username=username).first()
-
-    if user and check_password_hash(user.password, password):
-        session['user_id'] = user.id
-        return "Logged in successfully"
-    else:
-        return "Login failed"
-
+# ... (previous code)
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.init_app(app)
-        db.create_all()
     app.run(debug=True)
